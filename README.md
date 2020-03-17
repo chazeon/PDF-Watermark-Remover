a14337904eb1c57d2ada99c0c195c91a81d2abad
Chazeon <chenxing.luo@gmail.com>
2020-03-16 23:23:47 -0400

    Saving code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# PDF-Watermark-Remover
-Remove PDF watermarks from academic papers using PyPDF2
+# PDF Watermark Remover
+
+Remove PDF watermarks from academic papers using PyPDF2. It is not guarenteed to work but it works for me sometimes.
+
+## Usage
+
+```bash
+python3 remove.py input.pdf output.pdf
+```
---
+++ remove.py
@@ -0,0 +1,28 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from PyPDF2.pdf import ContentStream
+
+def removeWatermark(input_fname: str, output_fname: str):
+
+    with open(input_fname, "rb") as inputFile, open(output_fname, "wb") as outputFile:
+
+        reader = PdfFileReader(inputFile)
+        writer = PdfFileWriter()
+
+        for n in range(reader.numPages):
+
+            page = reader.getPage(n)
+            del page["/Contents"][-1]
+
+            writer.addPage(page)
+
+        writer.write(outputFile)
+
+if __name__ == "__main__":
+
+    import sys
+
+    if len(sys.argv) != 3:
+        raise RuntimeError("Arguments not correct!")
+
+    removeWatermark(sys.argv[1], sys.argv[2])
+
