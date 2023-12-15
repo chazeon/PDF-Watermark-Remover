83901d67859ab7095b7195a41c7989aa68b2c170
Chenxing Luo <chenxing.luo@gmail.com>
2023-12-15 01:57:19 +0000

    Allowing input file to be same as output file.

--- remove.py
+++ remove.py
@@ -2,7 +2,7 @@ from pypdf import PdfReader, PdfWriter
 
 def removeWatermark(input_fname: str, output_fname: str):
 
-    with open(input_fname, "rb") as input_file, open(output_fname, "wb") as output_file:
+    with open(input_fname, "rb") as input_file:
 
         reader = PdfReader(input_file)
         writer = PdfWriter()
@@ -14,14 +14,18 @@ def removeWatermark(input_fname: str, output_fname: str):
 
             writer.add_page(page)
 
+    with open(output_fname, "wb") as output_file:
+
         writer.write(output_file)
 
 if __name__ == "__main__":
 
     import sys
 
-    if len(sys.argv) != 3:
-        raise RuntimeError("Arguments not correct!")
-
-    removeWatermark(sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 2:
+        removeWatermark(sys.argv[1], sys.argv[1])
+    elif len(sys.argv) == 3:
+        removeWatermark(sys.argv[1], sys.argv[2])
+    else:
+        raise RuntimeError("Wrong number of arguments!")
 
